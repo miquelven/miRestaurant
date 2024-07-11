258bf5ab2b8ad39f399ff4fc78747c4f8439c3c9
miquelven <miquelven.silva@gmail.com>
2024-07-11 10:30:22 -0300

    feat: add banner component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <>
       <Header />
+      <Banner />
     </>
   );
 }
