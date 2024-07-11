1702a1ce5edaa63a3e6bfc882a8922f443281e30
miquelven <miquelven.silva@gmail.com>
2024-07-11 09:37:50 -0300

    feat: create linksdata file

---
+++ src/data/linksData.js
@@ -0,0 +1,29 @@
+const linksData = [
+  {
+    id: 1,
+    text: "Sobre",
+    href: "#sobre",
+  },
+  {
+    id: 2,
+    text: "Promoções",
+    href: "#promocoes",
+  },
+  {
+    id: 3,
+    text: "Menu",
+    href: "#menu",
+  },
+  {
+    id: 4,
+    text: "Prato especial",
+    href: "#especial",
+  },
+  {
+    id: 5,
+    text: "Avaliações",
+    href: "#avaliacoes",
+  },
+];
+
+export default linksData;
