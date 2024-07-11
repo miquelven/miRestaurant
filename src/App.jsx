fc42e64db205cc7f757e170d44c04cfb58cdfda1
miquelven <miquelven.silva@gmail.com>
2024-07-11 11:42:18 -0300

    feat: add about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <Header />
       <Banner />
+      <About />
     </>
   );
 }
