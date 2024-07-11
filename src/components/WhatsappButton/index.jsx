2d997e5dcb3ec07a0f20f07492c4767ac00070c7
miquelven <miquelven.silva@gmail.com>
2024-07-11 15:49:51 -0300

    style: set more opacity in hover effect

--- src/components/WhatsappButton/index.jsx
+++ src/components/WhatsappButton/index.jsx
@@ -2,7 +2,7 @@ import { FaWhatsapp } from "react-icons/fa";
 
 export default function WhatsappButton() {
   return (
-    <button className="transition-all duration-300 bg-[#f7c59f] w-full  min-w-8 max-w-44 py-3 flex justify-center text-black rounded-md shadow-lg shadow-black/40 mx-auto hover:bg-[#f7c59f]/80 max-sm:py-2">
+    <button className="transition-all duration-300 bg-[#f7c59f] w-full  min-w-8 max-w-44 py-3 flex justify-center text-black rounded-md shadow-lg shadow-black/40 mx-auto hover:bg-[#f7c59f]/60 max-sm:py-2">
       <FaWhatsapp className="w-7 h-7 max-sm:w-6 max-sm:h-6" />
     </button>
   );
