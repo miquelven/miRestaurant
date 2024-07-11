c11a98844eeeda3a3c819e62ed4fa383f99b37f5
miquelven <miquelven.silva@gmail.com>
2024-07-11 13:51:06 -0300

    feat: add qualityarea component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import "./App.css";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
 import About from "./components/About";
+import QualityArea from "./components/QualityArea";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
@@ -9,6 +12,7 @@ function App() {
       <Header />
       <Banner />
       <About />
+      <QualityArea />
     </>
   );
 }
