faa988aef10812a4ba5754df2166d6e472ebd9a7
miquelven <miquelven.silva@gmail.com>
2024-07-11 11:43:28 -0300

    feat: create sectiontextdecoration component

---
+++ src/components/SectionTextDecoration/index.jsx
@@ -0,0 +1,7 @@
+export default function SectionTextDecoration({ children }) {
+  return (
+    <span className="text-base font-semibold text-[#f7c59f] uppercase block text-center  [text-shadow:_1px_4px_0_rgb(0_0_0_/_100%)] max-lg:[text-shadow:_1px_3px_0_rgb(0_0_0_/_100%)] max-md:text-sm">
+      {children}
+    </span>
+  );
+}
