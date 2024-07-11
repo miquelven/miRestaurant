1187cc00ef3f818ba663c3a245d950b71ebbab19
miquelven <miquelven.silva@gmail.com>
2024-07-11 19:21:01 -0300

    feat: create reviewdata component

---
+++ src/data/reviewData.js
@@ -0,0 +1,44 @@
+import review1 from "../assets/imgs/review1.webp";
+import review2 from "../assets/imgs/review2.webp";
+import review3 from "../assets/imgs/review3.png";
+import review4 from "../assets/imgs/review4.webp";
+import review5 from "../assets/imgs/review5.webp";
+
+const reviewsData = [
+  {
+    id: 1,
+    name: "Ana Silva",
+    src: review1,
+    review:
+      "Ambiente acolhedor e comida deliciosa! Recomendo a todos que queiram uma experiência gastronômica inesquecível.",
+  },
+  {
+    id: 2,
+    name: "Carlos Pereira",
+    src: review2,
+    review:
+      "Serviço excelente e pratos de alta qualidade. Voltarei com certeza!",
+  },
+  {
+    id: 3,
+    name: "Mariana Costa",
+    src: review3,
+    review: "Os melhores chefs e um ambiente refrescante. Adorei minha visita!",
+  },
+  {
+    id: 4,
+    name: "João Souza",
+    src: review4,
+    review:
+      "Perfeito para festas e eventos. Minha família adorou a celebração que tivemos aqui!",
+  },
+  {
+    id: 5,
+    name: "Fernanda Lima",
+    src: review5,
+    review:
+      "A comida é maravilhosa e o atendimento impecável. Um dos meus restaurantes favoritos.",
+  },
+];
+
+export default reviewsData;
