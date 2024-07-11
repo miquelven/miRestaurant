351ec37097c7ebc66f754abf023518f687b6d9ce
miquelven <miquelven.silva@gmail.com>
2024-07-11 20:07:52 -0300

    refactor: toggle values

--- src/data/linksData.js
+++ src/data/linksData.js
@@ -6,13 +6,13 @@ const linksData = [
   },
   {
     id: 2,
-    text: "Menu",
-    href: "#menu",
+    text: "Alimentos",
+    href: "#alimentos",
   },
   {
     id: 3,
-    text: "Pratos",
-    href: "#pratos",
+    text: "Menu",
+    href: "#menu",
   },
   {
     id: 4,
