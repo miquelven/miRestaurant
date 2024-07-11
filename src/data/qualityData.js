da1d37b8ec872fd2403b2113aeefeed3b1e80fdc
miquelven <miquelven.silva@gmail.com>
2024-07-11 13:51:45 -0300

    feat: create qualitydata component

---
+++ src/data/qualityData.js
@@ -0,0 +1,23 @@
+import bread from "../assets/imgs/bread.jpg";
+import appetizers from "../assets/imgs/appetizers.png";
+import drinks from "../assets/imgs/drinks.png";
+
+const qualityData = [
+  {
+    id: 1,
+    title: "Café da manhã",
+    src: bread,
+  },
+  {
+    id: 2,
+    title: "Aperitivos",
+    src: appetizers,
+  },
+  {
+    id: 3,
+    title: "Bebidas",
+    src: drinks,
+  },
+];
+
+export default qualityData;
